39abb760fc7d52af73e51d130c5bd667d48186e9
Falling12 <senkcsani@gmail.com>
2025-03-01 17:35:53 +0100

    Fixed build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    esmExternals: false
+  },
 };
 
 export default nextConfig;
